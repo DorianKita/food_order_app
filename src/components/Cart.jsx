7865624faa21adc4164047a164fa0eebec9a164b
Dorian Kita <dorian.kita90@gmail.com>
2024-04-18 21:54:58 +0200

    added formatting and styles to caty Component

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -4,6 +4,7 @@ import CartContext from "../store/CartContext.jsx";
 import { currencyFormatter } from "../util/firmatting.js";
 import Button from "./UI/Button.jsx";
 import UserProgressContext from "../store/UserProgressContext.jsx";
+import CartItem from "./CartItem.jsx";
 
 export default function Cart() {
   const cartCtx = useContext(CartContext);
@@ -13,19 +14,23 @@ export default function Cart() {
     return (totalPrice += item.price * item.quantity);
   }, 0);
 
+  function handleCloseCart() {
+    userProgressCtx.hideCart();
+  }
+
   return (
-    <Modal className="cart" open={userProgressCtx.progress}>
+    <Modal className="cart" open={userProgressCtx.progress === "cart"}>
       <h2>Your Cart</h2>
       <ul>
         {cartCtx.items.map((item) => (
-          <li key={item.id}>
-            {item.name} - {item.quantity}
-          </li>
+          <CartItem key={item.id} {...item} />
         ))}
       </ul>
       <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
       <p className="modal-actions">
-        <Button textOnly>Close</Button>
+        <Button textOnly onClick={handleCloseCart}>
+          Close
+        </Button>
         <Button>Go to Checkout</Button>
       </p>
     </Modal>
---
+++ src/components/CartItem.jsx
@@ -0,0 +1,14 @@
+import { currencyFormatter } from "../util/firmatting.js";
+
+export default function CartItem({ name, quantity, price }) {
+  return (
+    <li className="cart-item">
+      <p>{`${name} - ${quantity} X ${currencyFormatter.format(price)}`}</p>
+      <p className="cart-item-actions">
+        <button>-</button>
+        <span>{quantity}</span>
+        <button>+</button>
+      </p>
+    </li>
+  );
+}
